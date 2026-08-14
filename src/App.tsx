d71a359dd227f00b71dd58b67ffc3829aed77dae
Stanley Osei-Wusu <107648937+stanleymay20@users.noreply.github.com>
2026-08-14 18:06:35 +0200

    Fix React Router v7 typecheck

--- src/App.tsx
+++ src/App.tsx
@@ -115,7 +115,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <BrowserRouter>
           <RouteMetadata />
           <AuthProvider>
             <CookieConsent />
