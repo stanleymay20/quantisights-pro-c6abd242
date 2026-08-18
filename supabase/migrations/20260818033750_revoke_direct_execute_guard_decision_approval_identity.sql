556017bef00417a5ea169ac733b01fabf6c00e3e
Stanley Osei-Wusu <107648937+stanleymay20@users.noreply.github.com>
2026-08-18 05:38:02 +0200

    security: revoke direct execute on approval guard trigger

---
+++ supabase/migrations/20260818033750_revoke_direct_execute_guard_decision_approval_identity.sql
@@ -0,0 +1,3 @@
+REVOKE ALL ON FUNCTION public.guard_decision_approval_identity() FROM PUBLIC;
+REVOKE ALL ON FUNCTION public.guard_decision_approval_identity() FROM anon;
+REVOKE ALL ON FUNCTION public.guard_decision_approval_identity() FROM authenticated;
